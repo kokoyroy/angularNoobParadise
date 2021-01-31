a618bc9aeb5a4c11159b36e415cfd1e831a7ef8e
kokoyroy <kokoyroy@gmail.com>
2021-01-31 08:38:26 +0200

    set the learning folder all together and took out of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
-import { FooterComponent } from './footer/footer.component';
-import { ChildComponent } from './main/child/child.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    MainComponent,
-    FooterComponent,
-    ChildComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
