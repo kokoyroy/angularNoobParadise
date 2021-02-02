012cb7d1dd92732f0aa7309503cb093f316ca77f
kokoyroy <kokoyroy@gmail.com>
2021-02-02 04:48:22 +0200

    started the shopping list

---
+++ src/app/shared/ingridient.model.ts
@@ -0,0 +1,6 @@
+export class ingridient {
+
+    constructor
+        (public name: string, public amount: number) {
+    }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ingridient } from '../shared/ingridient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingridients: ingridient[] = [
+    new ingridient('apples', 10),
+    new ingridient('tomatoes', 20)
+  ]
   constructor() { }
 
   ngOnInit(): void {
