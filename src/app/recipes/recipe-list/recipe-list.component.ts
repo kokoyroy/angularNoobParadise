5de72bf1e5bc0819c61ed16d590054f80bef0260
kokoyroy <kokoyroy@gmail.com>
2021-02-05 13:08:29 +0200

    made the details page and the click event of the recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,12 +8,16 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('photo','photo me fa asdsadsadsadsadasdssaasadaah  fisdofjosdfjodsfjojdfdsiifjsdefn dshodf oisgita apo to internet','https://libreshot.com/wp-content/uploads/2016/07/healthy-food.jpg'),
-    new Recipe('photo','photo me fa asdsadsadsadsadasdssaasadaah  fisdofjosdfjodsfjojdfdsiifjsdefn dshodf oisgita apo to internet','https://libreshot.com/wp-content/uploads/2016/07/healthy-food.jpg')
+    new Recipe('photo', 'photo me fa asdsadsadsadsadasdssaasadaah  fisdofjosdfjodsfjojdfdsiifjsdefn dshodf oisgita apo to internet', 'https://libreshot.com/wp-content/uploads/2016/07/healthy-food.jpg'),
+    new Recipe('photo', 'photo me fa asdsadsadsadsadasdssaasadaah  fisdofjosdfjodsfjojdfdsiifjsdefn dshodf oisgita apo to internet', 'https://upload.wikimedia.org/wikipedia/en/2/2c/Greek_food.jpeg')
   ]
+  @Output() recipeEmit = new EventEmitter<Recipe>()
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeEmit.emit(recipe);
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-
+  recipePassedToDetails: Recipe;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  recipeCatch(recipe: Recipe) {
+    this.recipePassedToDetails = recipe;
   }
 
 }
